cc97f900863e65db344aa4061ee3b650f9c3b61f
Alex <98610827+MindSetter77@users.noreply.github.com>
2025-02-09 02:50:54 +0100

    Item update

--- my-react-app/src/components/item/Item.tsx
+++ my-react-app/src/components/item/Item.tsx
@@ -1,6 +1,14 @@
 import { colors } from "../../colors"
 import { useEffect, useState } from "react"
 
+import StarIcon from '@mui/icons-material/Star';
+import StarHalfIcon from '@mui/icons-material/StarHalf';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
+
 interface Photo {
     id: number;
     item_id: number;
@@ -43,7 +51,7 @@ function Item() {
       }, []); // Pusty array zapewnia, że ten efekt wykona się tylko raz po załadowaniu komponentu
 
     return(
-        <div style={{width: '100%', height: 'calc(100vh - 64px)', backgroundColor: colors.background}}>
+        <div style={{display: 'flex', width: '100%', height: 'calc(100vh - 64px)', backgroundColor: colors.background, marginTop: '10px'}}>
             <div style={{display: 'flex', width: '500px', height: '400px'}}>
                 <div style={{display: 'flex', flexDirection: 'column', maxHeight: '400px', overflowY: 'auto', width: '105px'}}>
                     {photos.map((item, index) => (
@@ -56,6 +64,102 @@ function Item() {
                 </div>
                 
                 
+            </div>
+            <div style={{marginLeft: '25px', width: '500px'}}>
+                <div style={{width: '100%', backgroundColor: colors.primary, height: '30px', alignContent:' center'}}>
+                    <p style={{ marginLeft: '10px', color: 'white'}}>Na powitanie</p>
+
+                </div>
+                
+                <div style={{display: 'flex', height: '55px'}}>
+                    <p style={{fontSize: '40px', fontWeight: 'bold'}}>4,10 PLN</p>
+                    <p style={{color: 'red', marginTop: '24px', marginLeft: '10px',}}>36% zniżki</p>
+                </div>
+
+                <div style={{display: 'flex'}}>
+                    <p style={{fontSize: '13px', marginTop: '0px'}}>Najniższa cena w ciągu 30 dni przed obniżką cen. </p>
+                    <p style={{fontWeight: 'bold', fontSize:'13px', marginLeft: '3px'}}>6,41zł</p>
+                </div>
+
+                <p style={{color: 'gray', fontSize: '10px'}}>Cena zawiera podatek VAT</p>
+
+                <p style={{fontSize: '13px', fontWeight: 'bold'}}>List wygodne figi seksowna różowa marka VS bielizna koronkowa damska bliscy bielizna niski wzrost majtki damskie biustonosz stringi z kryształkami</p>
+                
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <StarIcon style={{color: 'orange', fontSize: '16px'}} />
+                    <StarIcon style={{color: 'orange', fontSize: '16px'}} />
+                    <StarIcon style={{color: 'orange', fontSize: '16px'}} />
+                    <StarHalfIcon style={{color: 'orange', fontSize: '16px'}}  />
+                    <StarOutlineIcon style={{color: 'orange', fontSize: '16px'}}  />
+                    <p style={{fontSize: '13px', fontWeight:'bold', marginLeft: '5px'}}  >4,8</p>
+                    <p style={{fontSize: '13px', marginLeft: '5px'}}  >| 747 Recenzje</p>
+                    <p style={{fontSize: '13px', marginLeft: '5px'}}  >| +4 000 Sprzedanych!</p>
+                </div>
+
+                <hr style={{marginTop: '5px', marginBottom: '5px'}}/>
+                
+                <p style={{fontSize: '15px', marginBottom: '5px'}}>Kolor: 5</p>
+
+                <div>
+                    {photos.map((item, index) => (
+                        <img key={index} src={item.src} onClick={() => setCurrentPhoto(item.src)} style={{width: '55px', marginRight: '10px', border: item.src === currentPhoto ? ('1px solid black') : ('1px dotted gray') , cursor: 'pointer'}} />
+                    ))}
+                </div>
+
+                <p style={{fontSize: '15px', marginBottom: '5px'}}>Rozmiar: XL</p>
+                <div style={{display: 'flex'}}>
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', border: '2px dotted gray', width: '60px', height: '38px', marginRight: '5px'}}> <p style={{color: 'gray'}}>M</p></div>
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', border: '2px dotted gray', width: '60px', height: '38px', marginRight: '5px'}}> <p style={{color: 'gray'}}>L</p></div>
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', border: '2px solid black', width: '60px', height: '38px'}}> <p>XL</p></div>
+                </div>
+
+                <p style={{fontSize: '15px', marginBottom: '5px', marginTop: '5px'}}>Sztuka: 1PC</p>
+                <div style={{display: 'flex'}}>
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', border: '2px solid black', width: '60px', height: '38px'}}> <p>XL</p></div>
+                </div>
+
+            </div>
+            <div>
+                <div style={{border: '1px solid gray', width: '400px', marginLeft: '10px', padding: '10px'}}>
+                    <div style={{display: 'flex'}}>
+                        <p style={{fontSize: '14px', fontWeight: 'bold'}}>Sprzedaż i wysyłka:</p>
+                        <p style={{fontSize: '14px', marginLeft: 'auto'}}>{'Shop1103999165 >'}</p>
+                    </div>
+
+                    <hr style={{marginTop: '5px', marginBottom: '5px'}}/>
+
+                    <div style={{display: 'flex'}}>
+                        <p style={{fontSize: '14px', fontWeight: 'bold'}}>Wysyłamy do:</p>
+                        <p style={{fontSize: '14px', marginLeft: 'auto'}}>{'Polska >'}</p>
+                    </div>
+                    
+                    <div style={{display: 'flex',  width: '100%', height: '40px', background: "linear-gradient(to bottom,rgb(255, 252, 95),rgb(254, 255, 208))", alignItems: 'center'}}>
+                        <div style={{display: 'flex', border: '2px solid orange', backgroundColor: 'rgb(255, 218, 95)', borderRadius: '5px', justifyContent: 'center', alignItems: 'center', height: '25px', width: '50px', marginLeft: '10px'}}>
+                            <p style={{fontSize: '11px', fontWeight: 'bold'}}>Choice</p>
+                        </div>
+                        <p style={{fontSize: '12px', fontWeight: 'bold', marginLeft: '10px'}}>Zobowiązanie BestMarket</p>
+                    </div>
+
+                    <div style={{padding: '10px'}}>
+                        <div style={{display: 'flex'}}>
+                            <LocalShippingIcon style={{fontSize: '20px', marginRight: '5px'}}/>
+                            <p style={{fontSize: '13px', fontWeight: 'bold'}}>Darmowa dostawa</p>
+                        </div>
+                        
+                        <div style={{display: 'flex'}}>
+                            <LocalPostOfficeIcon style={{fontSize: '20px', marginRight: '5px'}}/>
+                            <p style={{fontSize: '13px', fontWeight: 'bold'}}>Szybka dostawa</p>
+                        </div>
+
+
+                        <div style={{display: 'flex'}}>
+                            <HealthAndSafetyIcon  style={{fontSize: '20px', marginRight: '5px'}}/>
+                            <p style={{fontSize: '13px', fontWeight: 'bold'}}>Bezpieczeństwo i ochrona prywatności</p>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
             </div>
         </div>
     )
